da197a895bb5f072459eaa53fff9d91577726d73
Sushi6006 <lin.terry.x2@gmail.com>
2020-08-18 15:02:44 +1000

    rearranged files; created scenes with temp ctrls

---
+++ IdealPancake/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start() {
+        this.offset = this.transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called every frame, if the Behaviour is enabled.
+    // It is called after all Update functions have been called.
+    void LateUpdate() {
+        if (player != null) {
+            this.transform.position = this.offset + new Vector3(
+                player.transform.position.x,
+                player.transform.position.y,
+                player.transform.position.z);
+        }
+    }
+}
---
+++ IdealPancake/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 1;
+
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start() {
+        this.rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        // this.rigidbody.AddForce(movement * speed);
+        this.transform.position += movement * speed;
+    }
+}
