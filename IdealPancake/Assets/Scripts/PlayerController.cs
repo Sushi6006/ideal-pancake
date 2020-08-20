51dd6db25b3bb7b42920582c5c7ec5d22e6ff712
Sushi6006 <lin.terry.x2@gmail.com>
2020-08-20 18:49:58 +1000

    arrow rotates now

--- IdealPancake/Assets/Scripts/PlayerController.cs
+++ IdealPancake/Assets/Scripts/PlayerController.cs
@@ -3,25 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+
+    // player physics
     public float speed = 1;
 
-    private Rigidbody rigidbody;
+    // physics
+    private Vector3 movement;       // the movement of the pancake
+    private Rigidbody rb;
+    private bool isMoving = false;  // true if the pancake is moving
+
+    // arrow
+    public GameObject arrowObject;
+    public float arrowAngle;
+    private bool isArrowRotating = true;
+
+    // sounds
+    // TODO: sound to be added
+
+    // scores
+    // TODO: scores to be added
 
     // Start is called before the first frame update
     void Start() {
-        this.rigidbody = GetComponent<Rigidbody>();
+        this.rb = GetComponent<Rigidbody>();
+        this.isMoving = false;
+        this.isArrowRotating = true;
     }
 
     // Update is called once per frame
     void Update() {
+        if (this.isArrowRotating && (!this.isMoving)) {
+            this.arrowObject.transform.Rotate((this.arrowAngle * Time.deltaTime) * Vector3.back);
+        }
         
     }
 
     void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
         // this.rigidbody.AddForce(movement * speed);
         this.transform.position += movement * speed;
+        this.arrowObject.transform.position = this.transform.position;
     }
 }
